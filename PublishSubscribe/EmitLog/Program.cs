47c13011d2a48d79ce9b55d360a7bad38e559ac4
lirajon1988 <lira.jonas88@gmail.com>
2022-03-06 22:46:34 -0300

    Ajustando a estrutura dos projetos, incluindo projetos consoles de tutoriais do RabbitMQ

---
+++ PublishSubscribe/EmitLog/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace EmitLog
+{
+    /// <summary>
+    /// Programa para publicar mensagens para um exchange ao invés de mandar uma mensagem diretamente para a fila
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //Tipo de exchange Fanout é o que serve para entregar as mensagens para um exchange nesse executável
+                //durable: metadados da mensagem são armazenados em disco. Caso falso as mensagens são "transient" transitorias, gravando em memória
+                //channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout, durable: true);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                //Routingkey é ignorado quando o tipo de exchange informado é Fanout (não vamos direcionar a mensagem pra uma fila específica nesse caso)
+                channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+        }
+    }
+}
---
+++ PublishSubscribe/ReceiveLogs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ReceiveLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                // declara uma fila com nome gerado automaticamente pelo servidor
+                //var queueName = channel.QueueDeclare(queue: "").QueueName;
+
+
+                //autodelete: se verdadeiro vai deletar a fila quando o ultimo consumidor for fechado
+                //exclusive: se verdadeiro a fila só vai poder ser atualizada pela connection onde ela foi declarada. Se outra connection tentar ler dessa fila, Rabbit vai dar erro. São deletadas quando a conexão é fechada.
+                var queueName = channel.QueueDeclare(queue: "", autoDelete: false, exclusive:false).QueueName;
+
+                // a
+                channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    byte[] body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0}", message);
+                };
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
